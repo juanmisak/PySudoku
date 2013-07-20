d748cba0f59a015de3030d9c7b3b0273ff8f15fe
Juan Mite <juanmisak@hotmail.com>
2013-07-19 20:28:47 -0500

    developer.py from developer.ui, Resourse.qrc
    pase la ventana a python por medio del comando:
    pyrcc4 -o file.qrc file.py
    pyrcc4 -o resources.py resources.qrc

---
+++ developer.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file '/media/juan/D480EFE580EFCBD4/Users/Juan/Documents/GitHub/Sudoku-cpp/developer.ui'
+#
+# Created: Fri Jul 19 18:43:30 2013
+#      by: PyQt4 UI code generator 4.10
+#
+# WARNING! All changes made in this file will be lost!
+

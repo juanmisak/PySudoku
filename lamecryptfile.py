5d2baf5c0e07bc5eec3b10d2f22468f6545f7905
Ramón Carrillo <racarrillo91@gmail.com>
2013-07-20 00:15:12 -0500

    Open and save files using Lamecrypt
    Lamecrypt is a (lame) way to obfuscate the content in file.
    It is NOT real encryption.

---
+++ lamecryptfile.py
@@ -0,0 +1,38 @@
+import random
+
+class LamecryptFile:
+
+	def __init__(self, path, mode):
+		self.__f = open(path, mode)
+			
+		if mode == 'w':
+			# Generate the mask and write it to the file
+			self.__mask = random.randint(0, 255)
+			self.__f.write( chr(self.__mask) )
+		elif mode == 'r':
+			# Read the mask from the file
+			self.__mask = ord( self.__f.read(1) )
+
+
+	def read(self, size = -1):
+		data = self.__f.read(size)
+	
+		return self.__crypt(data)
+
+
+	def write(self, data):
+		encdata = self.__crypt(data)
+
+		self.__f.write(encdata)
+
+
+	def __crypt(self, data):
+		encdata = ''
+		for i in range( len( data ) ):
+			encdata += chr( ord(data[i]) ^ self.__mask )
+
+		return encdata
+
+
+	def close(self):
+		self.__f.close()
